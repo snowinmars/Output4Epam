82e24ee181219ded5d4783e133d66931d90ef199
snowonmars <stuff4work@yandex.ru>
2015-12-29 18:21:24 +0300

    minor update
    Чуть.

--- Output4Epam.PL.ASPNET/Scripts/MyScripts/JavaScript.js
+++ Output4Epam.PL.ASPNET/Scripts/MyScripts/JavaScript.js
@@ -167,12 +167,12 @@
 
 	function add(e) {
 		var group = $(".checkbox"),
-			form = $("form"),
-			i = 0,
+			form = $("form");
+			form.append('<input type="text" name="Types" value="">');
+		var i = 0,
 			type = form.children("[name=Types]"),
 			count = 0;
 
-		form.append('<input type="text" name="Types" value="">');
 		for (i = 0; i < group.length; ++i) {
 			if (group[i].checked) {
 				count += $(group[i]).data("enumvalue");
